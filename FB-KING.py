4960c607376ce7213fe94fff2eca1d0712f94fd2
Mahin Ahmed <106468659+FB-KING@users.noreply.github.com>
2022-10-07 10:09:05 +0600

    Add files via upload

---
+++ FB-KING.py
@@ -0,0 +1,33 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+
+        os.system('xdg-open https://www.facebook.com/FB.KING.MAHIN.NAME.TOH.SONSO')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from free import check_my_key
+ 
+        check_my_key()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from free import check_my_key
+ 
+ 
+        check_my_key()
+        
